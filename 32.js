9839b2a8485bc27146992d4bf53b6b810f67eb4e
JuanDanielVillegas <JuanDanielVillegas@github.com>
2021-11-16 17:32:07 -0700

    Programa que crea un arreglo de 20 elementos con nombres de personas. Muestra los elementos de la lista cada uno en una fila distinta

---
+++ 32.js
@@ -0,0 +1,26 @@
+let nombres=[
+    "Daniel",
+    "Noemí",
+    "Oscar",
+    "Pedro",
+    "Saul",
+    "Patricio",
+    "Alexis",
+    "Alejandro",
+    "Estefania",
+    "Sonia",
+    "Cesar",
+    "Carlos",
+    "Luis",
+    "Esteban",
+    "Jesus",
+    "Marcos",
+    "Silvia",
+    "Yahir",
+    "Raul",
+    "Jorge"
+];
+  
+for(let i=0; i<20; i++){
+    console.log(i+1 + "." + nombres[i]);
+}
