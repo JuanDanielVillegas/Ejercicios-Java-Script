78c2eb8b56235d9258b1019768f6a98d2438a415
JuanDanielVillegas <JuanDanielVillegas@github.com>
2021-11-16 20:41:46 -0700

    Programa que genera una matriz de 4 filas y 5 columnas con números aleatorios entre 1 y 100 y hace su matriz transpuesta

--- 35.js
+++ 35.js
@@ -1,18 +1,31 @@
 let numero;
-//let matriz = new Array(4);
+let matriz = new Array(4);
+let matrizT = new Array(4);
 function numAleatorio(){
   return Math.floor((Math.random()*100)+1);
 
 }
 
-for(let i=0; i<4; i++){
-    //matriz[i] = new Array(2);
+for(let i=0; i<5; i++){
+    matriz[i] = new Array(2);
     for(let j=0; j<5; j++){
-        //matriz[i][j]  = numAleatorio();
-        process.stdout.write(numAleatorio()+" ");
-
+        matriz[i][j] = numAleatorio();
     }
-    process.stdout.write(numAleatorio()+"\n");
 }
 
-//console.log(matriz);
+for(let i=0; i<5; i++){
+  matrizT[i] = new Array(2);
+  for(let j=0; j<4; j++){
+    matrizT[i][j] = matriz[j][i];
+    
+  }
+}
+
+console.log("Matriz");
+for(let i= 0; i<4; i++){
+  console.log(matriz[i]);
+}
+console.log("Matriz Transpuesta");
+for(let i= 0; i<5; i++){
+  console.log(matrizT[i]);
+}
